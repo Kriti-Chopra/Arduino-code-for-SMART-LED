26afa6520ca5e1bc93ef69beedeae25642ca15d2
Kriti-Chopra <37541206+Kriti-Chopra@users.noreply.github.com>
2018-09-10 14:09:56 +0530

    Add files via upload

---
+++ sketch_aug15a.ino
@@ -0,0 +1,23 @@
+char data;
+#define led 5
+#define fan 10
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+  pinMode(fan,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    data=Serial.read();
+    if(data=='1') digitalWrite(led,HIGH);
+    else if(data=='0') digitalWrite(led,LOW);
+    else if(data=='2') digitalWrite(fan,HIGH);
+    else if(data=='3') digitalWrite(fan,LOW);
+  }
+
+}
